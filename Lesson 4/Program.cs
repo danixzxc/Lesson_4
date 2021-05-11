334f3c6d3ed9820d49fa5cc3ac1bde69215c0d14
danixzxc <83531591+danixzxc@users.noreply.github.com>
2021-05-11 19:04:20 +0300

    Update Program.cs

--- Lesson 4/Program.cs
+++ Lesson 4/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson_4
         static void Main(string[] args)
         {
             //Гришель
-            //
+            //4. Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив. Создайте структуру Account, содержащую Login и Password.
             Menu.Start();
         }
 
